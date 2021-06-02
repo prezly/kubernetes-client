250d00254c851f7b6b4afb7d2fe02cf1f0f75dff
Ivan Voskoboinyk <ivan@prezly.com>
2021-06-02 14:24:11 +0300

    Add support for file-stored access tokens

--- src/KubernetesClientFactory.php
+++ src/KubernetesClientFactory.php
@@ -3,6 +3,7 @@
 namespace Prezly\KubernetesClient;
 
 use GuzzleHttp\Client as HttpClient;
+use InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 
 final class KubernetesClientFactory
@@ -55,10 +56,10 @@ final class KubernetesClientFactory
         ]);
     }
 
-    public function withCertificateAuthority(string $certificateAuthorityPath): self
+    public function withCertificateAuthority(string $certificateAuthority): self
     {
         return $this->withConfig([
-            'verify' => $certificateAuthorityPath,
+            'verify' => $certificateAuthority,
         ]);
     }
 
@@ -78,6 +79,10 @@ final class KubernetesClientFactory
 
     public function withAccessToken(string $accessToken): self
     {
+        if (substr($accessToken, 0, 7) === 'file://' || substr($accessToken, 0, 7) === 'data://') {
+            $accessToken = $this->readFile($accessToken);
+        }
+
         return $this->withHeaders([
             'Authorization' => "Bearer {$accessToken}",
         ]);
@@ -89,5 +94,16 @@ final class KubernetesClientFactory
             new HttpClient($this->config),
             $this->logger,
         );
+    }
+
+    private function readFile(string $uri): string
+    {
+        $contents = file_get_contents($uri);
+
+        if ($contents === false) {
+            throw new InvalidArgumentException("Failed to read contents from `{$uri}`.");
+        }
+
+        return $contents;
     }
 }
